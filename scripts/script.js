a074fd93f6d436fb8f7be27836950f1c079ea2b3
JoaoAlb2021 <joaorenato2015@gmail.com>
2022-04-13 01:55:08 -0500

    exercices

--- scripts/script.js
+++ scripts/script.js
@@ -1,122 +1,147 @@
-/////////////////JavaScript in browser/////////////
-
-///////Exercise 2////////////
-
-// let myElement = document.querySelector('.container')
-// console.log(myElement);
-
-///////Exercise 3////////////
-
-// let myElement = document.querySelector('.button')
-// myElement.style.backgroundColor = 'green';
-// myElement.addEventListener('submit', (event) => {
-//     event.preventDefault();
-// })
-
-///////Exercise 4////////////
-
-const textBox = document.querySelector('#new-todo')
-textBox.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    console.log(text);;
-})
-
-///////Exercise 5////////////
-
+const paragraphs = document.querySelectorAll('p')
+const myArray = Array.from(paragraphs)
 const todos = [];
-const createTodo = (text) => {
-    todos.push(text)
+
+const filters = {
+  searchTitle : '',
+  showFinished: false,
+  showUnfinished: false,
 }
-const form = document.querySelector('#new-todo')
-form.addEventListener('submit',(event)=>{
-    event.preventDefault()
-    const input = event.target.elements.text.value.trim()
-    if (input.length !== 0){
-        createTodo(input)
-        event.target.elements.text.value = ''
+
+const setFilters = (updates) => {
+  if (typeof updates.searchTitle === "string" ) {
+    filters.searchTitle = updates.searchTitle
+  }
+  if (typeof updates.showFinished === "boolean" ) {
+    console.log('test');
+    filters.showFinished = updates.showFinished
+  }
+  if (typeof updates.showUnfinished === "boolean" ) {
+    filters.showUnfinished = updates.showUnfinished
+  }
+} 
+
+const createTodos = (text) => {
+ todos.push({
+   title: text,
+   completed: false
+ })
+}
+
+const form = document.querySelector('form')
+
+function toggleTodo(title) {
+  const todo = todos.find((todo) => todo.title === title)
+  if (todo) {
+    todo.completed = !todo.completed
+  }
+}
+
+const generateTodoDOM = (todoObj) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type','checkbox')
+    checkbox.checked = todoObj.completed
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button--text')
+    
+    todoText.textContent = todoObj.title;
+    containerEl.appendChild(checkbox)
+    containerEl.appendChild(todoText)
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    
+    todoEl.appendChild(containerEl)
+    todoEl.appendChild(removeButton)
+    
+    checkbox.addEventListener('change',function (){
+      toggleTodo(todoObj.title)
+      renderTodos(todos)
+    })
+
+    removeButton.addEventListener('click',()=>{ 
+        removeTodo(todoObj.title)
+        renderTodos(todos)
+    })
+    return todoEl;
+}
+
+// Render application todos based on filters
+const renderTodos = (todos) => {
+  // filtered Todos
+  let filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(filters.searchTitle.toLowerCase()))
+  if(filters.showFinished && filters.showUnfinished) {
+    // do nothing
+  } else if(filters.showFinished) {
+    filteredTodos = filteredTodos.filter((todo) => todo.completed)
+  } else if(filters.showUnfinished) {
+    filteredTodos = filteredTodos.filter((todo) => !todo.completed)
+  }
+  
+  const todoList = document.querySelector('#todos')
+  todoList.innerHTML = ''
+
+  if (filteredTodos.length > 0) {
+      filteredTodos.forEach((todo) => {
+          todoList.appendChild(generateTodoDOM(todo))
+      })
+  } else {
+      const messageEl = document.createElement('p')
+      messageEl.classList.add('empty-message')
+      messageEl.textContent = 'There are no todos to show'
+      todoList.appendChild(messageEl)
+  }
+}
+function removeTodo (title) {
+    const todoIndex = todos.findIndex((todo)=> todo.title === title)
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
     }
-    console.log(todos);
+    
+}
+
+renderTodos(todos)
+
+document.querySelector("#search-text").addEventListener('input',(e)=>{
+  setFilters({searchTitle: e.target.value})
+  renderTodos(todos)
 })
 
-///////Exercise 6////////////
+document.querySelector('#show-finished').addEventListener('change',(e)=>{
+  setFilters({showFinished: e.target.checked})
+  renderTodos(todos)
+})
+document.querySelector('#show-unfinished').addEventListener('change',(e)=>{
+  setFilters({showUnfinished: e.target.checked})
+  renderTodos(todos)
+})
 
-const generateTodoDOM = (todo) => {
-    const todoE1 = document.createElement('label')
-    const containerE1 = document.createElement('div')
-    const todoText = document.createElement('span')
 
-    todoText.textContent = todo
-    containerE1.appendChild(todoText)
-    todoE1.appendChild(containerE1) 
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e);
+    let text = e.target.elements.text.value.trim()
+    if (text.length !== 0) {
+        createTodos(text) 
+        e.target.elements.text.value = ''
+        renderTodos(todos)
+    }
 
-    containerE1.classList.add('list-item_container')
-    todoE1.classList.add('list-item')
-    return todoE1
+})
+
+const saveTodosToLocalStorage = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
 
-///////Exercise 7////////////
-
-// const renderTodos = (todos) => {
-//     const todoList = document.querySelector('#todos')
-//     todoList.innerHTML = ''
-
-//     todos.forEach((todo) => {
-//         todoList.appendChild(generateTodoDOM(todo))
-//     });
-// }
-
-///////Exercise 8////////////
-
-const renderTodos = (todos) => {
-    const todoList = document.querySelector('#todos')
-    todoList.innerHTML = ''
-
-    if (todos.lenght !== 0)
-        todos.forEach((todo) => {
-            todoList.appendChild(generateTodoDOM(todo))
-        });
+function fetchTodosFromLocalStorage(){
+    const todosJSON = window.localStorage.getItem('todos')
+    if (todosJSON.length !== 0){
+        todos = JSON.parse(todosJSON)
+    }
     else {
-        const messageE1 = document.createElement(p)
-        message.classList.add('empty-message')
-        message.textContent = 'There are no todos to show'
-        todoList.appendChild(messageE1)
+        todos = []
     }
 }
-
-renderTodos(todos);
-
-// const container = document.querySelector('.container');
-// const allDiv = document.querySelectorAll('div')
-// console.log('this is the container',allDiv);
-
-// const button = document.querySelector('.button')
-// console.log(button);
-
-// const button = document.querySelector('.button')
-// button.addEventListener('submit',(event)=>{
-//     event.preventDefault();
-//     button.style.backgroundColor = 'green'
-// })
-
-
-
-
-
-
-
-
-
-/////////////////Objects in JavaScript//////////////////
-
-////////Excersice 11///////////
-
-// const todos =[];
-// const createTodo = (text) => {
-//     todos.push({
-//         title: text,
-//         completed: false
-//     })
-// }
-
-////////Excersice 12//////////
